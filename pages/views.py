de7681f57646722ba231cbe44930024a08737d42
Gegham Movses <gegham.movses@gmail.com>
2020-05-25 12:27:32 -0700

    add about page with template filter

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>hello world</h1>")
     return render(request, "home.html", {})
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "this is about us"
+    }
+    return render(request, "about.html", my_context)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view
+from pages.views import about_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
     path('admin/', admin.site.urls),
 ]
