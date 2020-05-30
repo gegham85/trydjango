149d89d4a2aab548c7d4c750bcd4909682098e98
Gegham Movses <gegham.movses@gmail.com>
2020-05-30 15:35:56 -0700

    add a new form

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -18,11 +18,12 @@ from django.urls import path
 
 from pages.views import home_view
 from pages.views import about_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('about/', about_view, name='about'),
-    path('product/', product_detail_view, name='product'),
+    path('create/', product_create_view),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
